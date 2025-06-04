d1c1e84c28e0b770c1859ecb80ac588353a9b965
Y W N <yewinnaing11605@gmail.com>
2025-06-04 10:02:29 +0630

    changed: domain

--- src/custom_knowledge.py
+++ src/custom_knowledge.py
@@ -33,7 +33,7 @@ headers = {
 }
 
 # API endpoint
-url = "http://188.166.231.0/api/mobile/v1/get-checkout-data"
+url = "https://myanmarbustickets.com/api/mobile/v1/get-checkout-data"
 
 # Optional payload for POST
 
